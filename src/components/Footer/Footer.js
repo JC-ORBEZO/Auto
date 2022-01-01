d5aece919e227ec31d6c810b6215dd80eb8f6a24
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-01-01 13:10:56 -0300

    commit de prueba despliegue 5.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,17 +15,7 @@ display:flex;
 justify-content:space-around;
 `;
 
-function Footer() {
-    /*
-    <a href="https://github.com/" className="text-dark"><i className="fab fa-github"></i></a>
-                    <a href="https://www.facebook.com/" className="text-primary"><i className="fab fa-facebook-square"></i></a>
-                    <a href="https://www.instagram.com/" className="text-danger"><i className="fab fa-instagram"></i></a>
-                    <a href="https://twitter.com/home" className="text-primary"><i className="fab fa-twitter"></i></a>
-                    <a href="https://www.twitch.tv/" className="text-secondary"><i className="fab fa-twitch"></i></a>
-                    <a href="https://www.youtube.com/" className="text-danger"><i className="fab fa-youtube"></i></a>
-                    <a href="https://www.linkedin.com/feed/" className="text-primary"><i className="fab fa-linkedin-in"></i></a>
-    
-    */
+function Footer() {    
     return (
         <AppContainer>
             <AppRow className="justify-content-center mt-5">
